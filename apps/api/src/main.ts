9e1de7928a0c50d8df96f3ac638434666bbbbbba
Vinicius Schneider <viniciusschneider4@gmail.com>
2026-01-08 22:34:26 -0300

    chore(api): adicionar swagger para documentação

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Finance App API')
+    .setDescription(
+      'API do Finance App (MVP) — controle financeiro com renda prevista e despesas por parcelas.',
+    )
+    .setVersion('1.0.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(3000);
 }
 bootstrap();
